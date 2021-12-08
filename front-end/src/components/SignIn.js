f82f20c5386be6575742a5bd9bd20f49136a24d0
Daniel <dandesantis14@gmail.com>
2021-12-07 21:35:39 -0500

    starting components

---
+++ front-end/src/components/SignIn.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function SignIn() {
+    return (
+        <div>
+            <form action="">
+                <input typ='text' placeholder='Username/email'></input>
+                <input typ='text' placeholder='Password'></input>
+                <button type='submit'>Submit</button>
+                <button type='submit'>Make Playlist</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignIn

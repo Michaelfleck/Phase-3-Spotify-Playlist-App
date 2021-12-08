632df16b02c8f6a1129ae7b131618451833684de
mfleck <michael.j.fleck@gmail.com>
2021-12-07 23:15:54 -0600

    Credentials testing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,33 @@
-import React from  'react'
-import './App.css';
+import React, {useState, useEffect} from  'react'
+import { Credentials } from './components/Credentials'
+import axios from 'axios';
 
 function App() {
+
+  const spotify = Credentials();
+
+  const [token, setToken] = useState('')
+
+  useEffect( () => {
+
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)
+    },
+    data: 'grant_type=client_credentials',
+    method: 'POST'
+  })
+  .then(tokenResponse => {
+    console.log("testing tokenResponse")
+    console.log(tokenResponse.data.access_token);
+    setToken(tokenResponse.data.access_token)
+    console.log("-----------------------")
+  });
+  }, []);
+
+
+
   return (
     <div>
       Hello
---
+++ front-end/src/components/Credentials.js
@@ -0,0 +1,15 @@
+const Credentials = () => {
+
+    return {
+        // Dan's Credentials
+        ClientId: '37746623aa164deaab96511ea8090627',
+        ClientSecret: '3e9ab03d09084f498befb46cf4ffaa9b'
+
+        // Michael's Credentials
+        // ClientId: 'b997a3c051c84b9484b3afcbf6604a9a',
+        // ClientSecret: 'b19be9a4914a4ba28be0b6ffe59481b9'
+    }
+}
+
+
+export { Credentials };

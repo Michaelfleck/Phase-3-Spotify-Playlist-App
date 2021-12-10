c291e289ef54fbf24f8d333bca14afc5e75c264b
Michael Fleck <michael@pop-os.localdomain>
2021-12-10 01:38:40 -0600

    Fixed

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -23,9 +23,12 @@ class ApplicationController < Sinatra::Base
 
         spotify_id = body["id"]
         display_name = body["display_name"]
+        image = body["images"][0]["url"]
+        url = body["external_urls"]["spotify"]
 
 
-        User.create("access_token":params[:access_token], "spotify_id":spotify_id, "display_name":display_name)
+
+        User.create("access_token":params[:access_token], "spotify_id":spotify_id, "display_name":display_name, "image":image, "spotify_link":url)
 
         body.to_json
     end
@@ -56,6 +59,28 @@ class ApplicationController < Sinatra::Base
       {}.to_json
     end
 
+    post '/playlist' do
+      request.body.rewind
+      request_payload = JSON.parse request.body.read
+      puts request_payload
+      id = request_payload["id"]
+      uris = request_payload["uris"]
+      resp = postStuff(@@token, "https://api.spotify.com/v1/users/#{id}/playlists", { public: true, name: "foobar" })
+      puts resp
+      puts postStuff(@@token, "https://api.spotify.com/v1/playlists/#{resp['id']}/tracks", { uris: uris })\
+      {}.to_json
+    end
+
+    delete '/users' do
+      User.destroy_all();
+      {}.to_json
+    end
+
+    delete '/songs' do
+      Song.destroy_all();
+      {}.to_json
+    end
+
     get '/songs' do
       songlist = []
       User.all.ids.each do |userId|
@@ -72,6 +97,25 @@ class ApplicationController < Sinatra::Base
       request = Net::HTTP::Get.new(uri)
       request.content_type = "application/json"
       request["Authorization"] = "Bearer #{token}"
+      
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      JSON.parse(response.body)
+  
+    end
+
+    def postStuff (token, spotify_url, body)
+      uri = URI.parse(spotify_url)
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = "application/json"
+      request["Authorization"] = "Bearer #{token}"
+      request.body = body.to_json
       
       req_options = {
         use_ssl: uri.scheme == "https",
